9d6540930f5881a6e340dde87fcd36c9a144d8fe
Nicolas Klein <nicolasklein1998@gmail.com>
2022-08-27 15:24:36 +0200

    Animated shadow on socials

--- components/Socials.js
+++ components/Socials.js
@@ -7,9 +7,11 @@ import { IconContext } from "react-icons";
 /// LinkedIn, Behance, Github, Twitter
 
 const Socials = () => {
-    return <IconContext.Provider value={{ color: 'black', className: 'react-icons' }}>
+    return <IconContext.Provider value={{className: styles.Icons }}>
     <div className={styles.SocialsBar}>
-        <BsLinkedin size={40}  />
+        <a href="/auth/google">
+            <BsLinkedin size={40}/>
+        </a>
         <BsBehance size={40}  />
         <BsGithub size={40}  />
         <BsTwitter size={40}  />
