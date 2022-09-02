831a6f510a634e440581c1f73320493d2692a367
Nicolas Klein <nicolasklein1998@gmail.com>
2022-09-03 01:34:42 +0200

    Restyled navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
             "0 35px 35px rgba(0, 0, 0, 0.25)",
             "0 45px 65px rgba(0, 0, 0, 0.15)",
         ],
+        "nav": "0 4px 30px rgba(0, 0, 0, 0.1)"
       },
       borderWidth: {
         DEFAULT: "1px",
