68cb88f6a4efa7ad1839748757df8d00d8e0a581
Nicolas Klein <nicolasklein1998@gmail.com>
2023-09-17 12:49:20 +0200

    Added page structure and recent tracks sidebar

---
+++ components/TrackSmall/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { BsPlay, BsTwitter } from "react-icons/bs";
+import ITrack from "../../interfaces/ITrack";
+import Zoomed from "../Zoomed";
+
+const TrackSmall = (props: { track: ITrack }) => {
+    return (
+        <div className="group flex gap-2 rounded-xl transition ease-in-out hover:scale-110 hover:bg-white">
+            <Image
+                className="rounded-xl transition ease-in-out group-hover:rounded-none group-hover:rounded-l-lg group-hover:brightness-100"
+                alt={props.track.title}
+                src={props.track.cover}
+                width={64}
+                height={64}
+            />
+            <div className="flex flex-col justify-center">
+                <a
+                    href={props.track.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="mr-2 max-w-[250px] truncate text-lg font-bold group-hover:text-black"
+                >
+                    {props.track.title}
+                </a>
+                <a
+                    href={props.track.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="max-w-[250px] truncate text-lg font-thin group-hover:text-black"
+                >
+                    {props.track.artists[0].name}
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default TrackSmall;
---
+++ pages/music.tsx
@@ -0,0 +1,152 @@
+import React from "react";
+import { DateTime, Interval } from "luxon";
+import { getRecentTracks, getTopTracks } from "../lib/spotify";
+import { GetStaticProps } from "next";
+import ITrack from "../interfaces/ITrack";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import CustomButton from "../components/SayHello/CustomButton";
+import Zoomed from "../components/Zoomed";
+import Wobbly from "../components/Wobbly";
+import Image from "next/image";
+import memoji from "../public/assets/memoji.png";
+import TrackSmall from "../components/TrackSmall";
+
+export const getStaticProps: GetStaticProps = async () => {
+    const tracks = await getRecentTracks();
+
+    return {
+        props: {
+            top_tracks: tracks,
+            refreshed: DateTime.now().valueOf(),
+            year: DateTime.now().year,
+        },
+        revalidate: 600,
+    };
+};
+
+interface Props {
+    top_tracks: ITrack[];
+    refreshed: number;
+    year: number;
+}
+
+const Music = (props: Props) => {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <title id="title">music is 💚</title>
+                <meta
+                    name="Description"
+                    content="👋 Hey this is what I listen to."
+                />
+            </Head>
+            <Layout>
+                <div className="relative flex min-h-screen animate-fade-in-up items-center">
+                    <div className="w-full">
+                        <div className="animate-pulse-slow-3">
+                            <div
+                                style={{ filter: "saturate(2)" }}
+                                className=" dark:opacity-1000 top-90 absolute -left-[800px] right-0 z-0 ml-auto mr-auto h-80 w-72 origin-center animate-spin-slow opacity-30 dark:opacity-90"
+                            >
+                                <div
+                                    className="absolute -left-36 -top-36 h-96 w-96 animate-pulse-slow-3 rounded-full bg-primary-500 opacity-90"
+                                    style={{ filter: "blur(180px)" }}
+                                />
+                                <div
+                                    className="absolute -left-12 top-36 h-64 w-64 animate-pulse-slow-5 rounded-full bg-emerald-300 opacity-80"
+                                    style={{ filter: "blur(120px)" }}
+                                />
+                                <div
+                                    className="absolute left-44 top-12 h-72 w-72 animate-pulse-slow-7 rounded-full bg-sky-500 opacity-60"
+                                    style={{ filter: "blur(120px)" }}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="animate-pulse-slow-3">
+                            <div
+                                style={{ filter: "saturate(2)" }}
+                                className=" dark:opacity-1000 top-90 absolute left-0 right-0 z-0 ml-auto mr-auto h-80 w-72 origin-center animate-spin-slow opacity-30 dark:opacity-50"
+                            >
+                                <div
+                                    className="absolute -left-36 -top-36 h-96 w-96 animate-pulse-slow-3 rounded-full bg-primary-500 opacity-90"
+                                    style={{ filter: "blur(180px)" }}
+                                />
+                                <div
+                                    className="absolute -left-12 top-36 h-64 w-64 animate-pulse-slow-5 rounded-full bg-emerald-300 opacity-80"
+                                    style={{ filter: "blur(120px)" }}
+                                />
+                                <div
+                                    className="absolute left-44 top-12 h-72 w-72 animate-pulse-slow-7 rounded-full bg-sky-500 opacity-60"
+                                    style={{ filter: "blur(120px)" }}
+                                />
+                            </div>
+                        </div>
+
+                        <div
+                            style={{ filter: "saturate(2)" }}
+                            className="top-90 dark:opacity:50 absolute right-10 z-0 h-72 w-72 origin-center animate-spin-slow opacity-30 dark:opacity-80"
+                        >
+                            <div
+                                className="absolute -right-36 -top-36 h-96 w-96 animate-pulse-slow-7 rounded-full bg-sky-500 opacity-90"
+                                style={{ filter: "blur(180px)" }}
+                            />
+                            <div
+                                className="absolute -right-12 top-36 h-64 w-64 animate-pulse-slow-5 rounded-full bg-primary-500 opacity-80"
+                                style={{ filter: "blur(120px)" }}
+                            />
+                            <div
+                                className="absolute right-20 top-0 h-72 w-72 animate-pulse-slow-3 rounded-full bg-emerald-500 opacity-60"
+                                style={{ filter: "blur(120px)" }}
+                            />
+                        </div>
+                        <div className="relative z-20 flex h-full flex-col items-center justify-center md:flex-row">
+                            <div className="mr-24 pt-4 sm:p-0">
+                                <h1 className="mb-2 text-5xl font-bold text-emerald-200 opacity-60">
+                                    Recents
+                                </h1>
+                                <div className="z-0 flex w-80 flex-col gap-4 rounded-xl bg-emerald-200 bg-opacity-20 bg-clip-padding p-2 backdrop-blur-2xl backdrop-filter">
+                                    {props.top_tracks
+                                        .slice(0, 6)
+                                        .map((track: ITrack, index) => (
+                                            <TrackSmall
+                                                key={index}
+                                                track={track}
+                                            />
+                                        ))}
+                                </div>
+                            </div>
+                            <div className="pb-48 md:pb-0">
+                                <h2 className="m-0 mb-2 mt-6 text-4xl font-normal sm:mb-2 sm:mt-0 sm:text-6xl">
+                                    Hey this is my
+                                </h2>
+                                <h1
+                                    className="m-0 bg-gradient-to-br from-emerald-500 to-green-300 bg-clip-text 
+                                        text-6xl font-bold text-transparent sm:text-[130px]"
+                                >
+                                    Spotify
+                                </h1>
+                                <div className="flex flex-col items-start justify-start gap-2 pb-10 pt-4 sm:flex-row sm:pl-2">
+                                    <CustomButton
+                                        title="Contact"
+                                        secondary={false}
+                                    />
+                                    <div className="hidden sm:block">
+                                        <CustomButton
+                                            title={"Go to my Github"}
+                                            secondary={true}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    );
+};
+
+export default Music;
